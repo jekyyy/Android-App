a099b1b6fdf409ba90d7a5ccd3214f9f7eac1956
Leslie <lesliekhang93@gmail.com>
2018-10-15 21:46:25 +1100

    gggg

---
+++ app/src/main/java/com/example/novaa/infs3634assignment/QuestionLibrary.java
@@ -0,0 +1,21 @@
+package com.example.novaa.infs3634assignment;
+
+public class QuestionLibrary {
+
+    private String mQuestions [] = {
+            "Which of the following is not an OOP concept in Java?",
+            "When does method overloading is determined?",
+            "When Overloading does not occur?",
+            "Which concept of Java is a way of converting real world objects in terms of class?",
+            "Which concept of Java is achieved by combining methods and attribute into a class?",
+            "Method overriding is combination of inheritance and polymorphism?",
+
+    };
+
+    private String mChoices [] [] = {
+            {"Inheritance", "Encapsulation", "Polymorphism", "Compilation"},
+            {"At runtime", "At compile time", "At coding time", "At execution time"}
+    };
+
+    private String mCorrectAnswers[] = {}
+}
