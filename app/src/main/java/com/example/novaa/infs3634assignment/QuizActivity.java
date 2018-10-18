e62b60b178a5fb7713219752ab0c95799f933a8e
Leslie <lesliekhang93@gmail.com>
2018-10-18 18:51:33 +1100

    missing 2 letters for API KEY

--- app/src/main/java/com/example/novaa/infs3634assignment/QuizActivity.java
+++ app/src/main/java/com/example/novaa/infs3634assignment/QuizActivity.java
@@ -1,149 +1,157 @@
 package com.example.novaa.infs3634assignment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Random;
+
+//https://www.youtube.com/watch?v=4g1_UH_6VQc - used as a source for the QuizActivity logic
 
 public class QuizActivity extends AppCompatActivity {
 
-    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+    Button answer1, answer2, answer3, answer4;
 
-    private TextView mScoreView;
-    private TextView mQuestionView;
-    private Button mButtonChoice1;
-    private Button mButtonChoice2;
-    private Button mButtonChoice3;
+    TextView score, question;
 
-
+    private Questions mQuestions = new Questions();
     private String mAnswer;
     private int mScore = 0;
-    private int mQuestionNumber = 0;
+    private int mQuestionLength = mQuestions.mQuestions.length;
+
+    Random r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mScoreView = (TextView)findViewById(R.id.score);
-        mQuestionView = (TextView)findViewById(R.id.question);
-        mButtonChoice1 = (Button)findViewById(R.id.choice1);
-        mButtonChoice2 = (Button)findViewById(R.id.choice2);
-        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+        r = new Random();
 
-        updateQuestion();
+        answer1 = (Button) findViewById(R.id.answer1);
+        answer2 = (Button) findViewById(R.id.answer2);
+        answer3 = (Button) findViewById(R.id.answer3);
+        answer4 = (Button) findViewById(R.id.answer4);
 
-        //Creating Button listener for all button1
+        score = (TextView) findViewById(R.id.score);
+        question = (TextView) findViewById(R.id.question);
 
-        mButtonChoice1.setOnClickListener(new View.OnClickListener() {
+        score.setText("Score:" + mScore);
+
+        updateQuestion(r.nextInt(mQuestionLength));
+
+
+
+        answer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(answer1.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score:" + mScore);
+                    updateQuestion(r.nextInt(mQuestionLength));
 
-                if (mButtonChoice1.getText() == mAnswer){
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
 
-                    //Toast message = feedback for the user when question is right or wrong
-
-                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
+                } else {
+                    gameOver();
 
 
                 }
+
             }
         });
 
-        //Creating Button listener for all button2
-
-        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+        answer2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
+                if(answer2.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score:" + mScore);
+                    updateQuestion(r.nextInt(mQuestionLength));
 
-
-                if (mButtonChoice2.getText() == mAnswer){
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
-
-                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
+                } else {
+                    gameOver();
                 }
+
+
+
             }
         });
 
-        //Creating Button listener for all button3
-
-
-        mButtonChoice3.setOnClickListener(new View.OnClickListener() {
+        answer3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(answer3.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score:" + mScore);
+                    updateQuestion(r.nextInt(mQuestionLength));
 
-                if (mButtonChoice3.getText() == mAnswer) {
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
-
-                    //Toast message = feedback for the user when question is right or wrong
-
-                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
-
-
+                } else {
+                    gameOver();
                 }
+
             }
         });
 
-        mButtonChoice3.setOnClickListener(new View.OnClickListener() {
+        answer4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(answer4.getText() == mAnswer) {
+                    mScore++;
+                    score.setText("Score:" + mScore);
+                    updateQuestion(r.nextInt(mQuestionLength));
 
-                if (mButtonChoice3.getText() == mAnswer) {
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
-
-                    //Toast message = feedback for the user when question is right or wrong
-
-                    Toast.makeText(QuizActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(QuizActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
-
-
+                } else {
+                    gameOver();
                 }
+
             }
         });
 
 
     }
 
-        private void updateQuestion() {
-            mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
-            mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
-            mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
-            mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
+    private void updateQuestion(int num) {
+        question.setText(mQuestions.getQuestion(num));
+        answer1.setText(mQuestions.getChoice1(num));
+        answer2.setText(mQuestions.getChoice2(num));
+        answer3.setText(mQuestions.getChoice3(num));
+        answer4.setText(mQuestions.getChoice4(num));
+
+        mAnswer = mQuestions.getCorrectAnswer((num));
 
 
-            mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
-            mQuestionNumber++;
 
-        }
+    }
 
+    private void gameOver() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(QuizActivity.this);
+        alertDialogBuilder
+                .setMessage("Game Over! Your score is " + mScore + " points.")
+                .setCancelable(false)
+                .setPositiveButton("NEW GAME",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(getApplicationContext(),QuizActivity.class));
+                                finish();
 
-    private void updateScore(int point) {
-        mScoreView.setText("" + mScore);
+                            }
+                        })
+                .setNegativeButton("HOME",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
